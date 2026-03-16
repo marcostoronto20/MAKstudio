8958391825fcd3996c3fa4ddc7232d623ebb2b14
marcostoronto20 <marcostoronto20@gmail.com>
2026-03-16 15:59:27 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
   const { t } = useLanguage();
 
   return (
-    <section className="relative min-h-[100svh] md:h-screen flex flex-col justify-start pt-24 md:pt-32 px-4 md:px-6 overflow-hidden">
+    <section className="relative w-full min-h-[100svh] md:h-screen flex flex-col justify-start pt-24 md:pt-32 px-4 md:px-6 overflow-hidden">
       {/* Full Screen Background Image */}
       <div className="absolute inset-0 z-0 bg-black">
         {/* Desktop background */}
